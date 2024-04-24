2696f6e29f72a64d9e2cf39e5d7c49dc45c5ddb6
mdJasim2023 <jasim127md@gmail.com>
2024-04-24 21:50:38 +0530

    2nd commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -14,23 +14,35 @@ const Main = () => {
                 <p>How can I help you Today</p>
             </div>
             <div className="cards">
-                <div className="card">
-                    <p>sakjfnkdnsf</p>
+                <div className="carrd">
+                    <div>Suggest Beautiful places to see on an upcoming road trip.</div>
                     <img src={assets.compass_icon} alt="" />
                 </div>
-                <div className="card">
-                    <p>sakjfnkdnsf</p>
+                <div className="carrd">
+                    <div>Briefly summarize this concept : urban planing.</div>
                     <img src={assets.bulb_icon} alt="" />
                 </div>
-                <div className="card">
-                    <p>sakjfnkdnsf</p>
+                <div className="carrd">
+                    <div>Brainstrom team bonding activities for our worl retreat.</div>
                     <img src={assets.message_icon} alt="" />
                 </div>
-                <div className="card">
-                    <p>sakjfnkdnsf</p>
+                <div className="carrd">
+                    <div>Improve the readability of the following code.</div>
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+        </div>
+
+        <div className="main-bottom">
+            <div className="search-box">
+                <input type="text" placeholder='Enter a prompt here'/>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+            </div>
+            <p className="bottom-info">
+                This Application display inaccurate info, including about people, so double-check its responses. Your privacy and this App.
+            </p>
         </div>
     </div>
   )
